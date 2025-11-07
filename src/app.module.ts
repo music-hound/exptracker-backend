01f03dee80234c8c573807ae4474e305728e5c01
Igor <musichound.ge@gmail.com>
2025-11-07 12:07:14 +0500

    db conn fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,17 +17,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: "postgres",
-        host: configService.get("DB_HOST"),
-        port: configService.get("DB_PORT"),
-        username: configService.get("DB_USERNAME"),
-        password: configService.get("DB_PASSWORD"),
-        database: configService.get("DB_NAME"),
-        synchronize: true,
-        entities: [__dirname + "/**/*.entity{.ts,.js}"],
-      }),
       inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: "postgres",
+        url: config.get<string>("DATABASE_URL"),
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
     }),
   ],
   controllers: [AppController],
