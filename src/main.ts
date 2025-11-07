88b8587307e6fbafb9a43be666ab9406aa5a33d0
Igor <musichound.ge@gmail.com>
2025-11-07 14:56:03 +0500

    delete prefix

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("/api-docs", app, document);
 
-  app.setGlobalPrefix("api");
+  // app.setGlobalPrefix("api");
   app.enableCors();
   await app.listen(3030);
 }
